d238b67ef07b9fef234eda414bada847a40aa5c9
Paulo Fierro <paulo@paulofierro.com>
2022-03-29 19:33:58 -0500

    Muted log

--- Sources/PFToolbox/Extensions/Array.swift
+++ Sources/PFToolbox/Extensions/Array.swift
@@ -11,7 +11,6 @@ public extension Array {
     /// Returns an element at a specific index, or nil if the index is out of bounds.
     subscript(safeIndex index: Int) -> Element? {
         guard index >= startIndex, index < endIndex else {
-            log.error("Index out of bounds was safely avoided: \(index)")
             return nil
         }
         return self[index]
