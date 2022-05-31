d6cb6fb171f71287e068d4c85ca617282ba52dfd
Paulo Fierro <paulo@paulofierro.com>
2022-05-31 10:20:51 -0500

    Renamed endpoint properties

--- Sources/PFToolbox/Networking/Services/Endpoint.swift
+++ Sources/PFToolbox/Networking/Services/Endpoint.swift
@@ -10,7 +10,7 @@ import Foundation
 public protocol EndPoint {
     var baseURL: URL { get }
     var path: String { get }
-    var httpMethod: HTTPMethod { get }
+    var method: HTTPMethod { get }
     var task: HTTPTask { get }
-    var headers: HTTPHeaders? { get }
+    var headers: HTTPHeaders { get }
 }
