c703f4473a9cd37193b5a2d546ccd02c7615ded9
Paulo Fierro <paulo@paulofierro.com>
2022-05-31 09:47:42 -0500

    Added Dictionary extension

---
+++ Sources/PFToolbox/Extensions/Dictionary.swift
@@ -0,0 +1,19 @@
+//
+//  Dictionary.swift
+//  
+//
+//  Created by Paulo Fierro on 31/5/22.
+//
+
+import Foundation
+
+extension Dictionary {
+    /// Adds/overwrites all the values from the new dictionary
+    mutating func merge(dict: [Key: Value]?) {
+        guard let dict = dict else { return }
+        
+        for (k, v) in dict {
+            updateValue(v, forKey: k)
+        }
+    }
+}
---
+++ Tests/PFToolboxTests/Extensions/DictionaryTests.swift
@@ -0,0 +1,29 @@
+//
+//  DictionaryTests.swift
+//  
+//
+//  Created by Paulo Fierro on 31/5/22.
+//
+
+import XCTest
+@testable import PFToolbox
+
+final class DictionaryTests: XCTestCase {
+    
+    func testMerging() async throws {
+        var one = ["a": 1, "b": 2]
+        let two = ["c": 3]
+        one.merge(dict: two)
+        XCTAssertEqual(one["a"], 1)
+        XCTAssertEqual(one["b"], 2)
+        XCTAssertEqual(one["c"], 3)
+    }
+    
+    func testMergingOverwrite() async throws {
+        var one = ["a": 1, "b": 2]
+        let two = ["b": 3]
+        one.merge(dict: two)
+        XCTAssertEqual(one["a"], 1)
+        XCTAssertEqual(one["b"], 3)
+    }
+}
