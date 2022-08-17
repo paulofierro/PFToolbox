be8724db5d301f9b85e8326aacb12f0f240398fa
Paulo Fierro <paulo@paulofierro.com>
2022-08-17 13:53:48 -0500

    Added value constructable util

---
+++ Sources/PFToolbox/Utils/ValueConstructable.swift
@@ -0,0 +1,40 @@
+//
+//  ValueConstructable.swift
+//  
+//
+//  Created by Paulo Fierro on 17/8/22.
+//
+
+public protocol ValueConstructable {
+    // Defines the value type that is stored
+    associatedtype Item
+    
+    /// The default value to use when none can be found for the value that was provided.
+    static func defaultValue() -> Self
+    
+    /// Returns the case for this enum that can be constructed from the value.
+    static func from(_ value: Item) -> Self
+    
+    /// Returns the case for this enum that can be constructed from the optional value.
+    static func from(_ value: Item?) -> Self
+}
+
+// Allows for generic construction of value-based enums from raw values
+public extension ValueConstructable where Self: RawRepresentable, Self.RawValue == Item {
+    /// Returns a case based on the raw value passed in, or the default value if none is found.
+    static func from(_ value: Item) -> Self {
+        if let object = Self(rawValue: value) {
+            return object
+        }
+        log.error("Could not find a \(Self.self) for \"\(value)\"")
+        return defaultValue()
+    }
+
+    /// Returns a case based on the optional, raw value passed in, or the default value if none is found.
+    static func from(_ value: Item?) -> Self {
+        guard let value = value else {
+            return defaultValue()
+        }
+        return from(value)
+    }
+}
---
+++ Tests/PFToolboxTests/Utils/ValueConstructableTests.swift
@@ -0,0 +1,46 @@
+import XCTest
+@testable import PFToolbox
+
+class ValueConstructableTests: XCTestCase {
+    enum StringEnum: String, ValueConstructable {
+        case one, two, three
+
+        static func defaultValue() -> Self {
+            .one
+        }
+    }
+
+    enum IntEnum: Int, ValueConstructable {
+        case one, two, three
+
+        static func defaultValue() -> Self {
+            .one
+        }
+    }
+
+    func testCreatingFromString() throws {
+        let value = StringEnum.from("two")
+        XCTAssertEqual(value, StringEnum.two)
+    }
+
+    func testCreatingFromInt() throws {
+        let value = IntEnum.from(1)
+        XCTAssertEqual(value, IntEnum.two)
+    }
+
+    func testNilDefaultValue() throws {
+        XCTAssertEqual(StringEnum.from(nil), StringEnum.one)
+        XCTAssertEqual(IntEnum.from(nil), IntEnum.one)
+    }
+
+    func testEmptyDefaultValue() throws {
+        XCTAssertEqual(StringEnum.from(""), StringEnum.one)
+        XCTAssertEqual(IntEnum.from(nil), IntEnum.one)
+    }
+    
+    func testOptionalDefaultValue() throws {
+        let value: String? = ""
+        XCTAssertEqual(StringEnum.from(value), StringEnum.one)
+        XCTAssertEqual(IntEnum.from(nil), IntEnum.one)
+    }
+}
