4625d5aed43dccf418465148051fc8f66ca18f6e
Paulo Fierro <paulo@paulofierro.com>
2023-01-20 18:57:33 -0500

    Updated coverage outputter

--- coverage.sh
+++ coverage.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 #
-# Outputs the code coverage report as a summary ("report") or JSON ("export")
+# Outputs the code coverage report as a printed summary and JSON
 # Author: Paulo Fierro <paulo@paulofierro.com>
-# Date: July 19, 2022
+# Date: Jan 20, 2023
 #
-TYPE=$1
-if [ -z "$TYPE" ]
-then
-    echo "Please supply an output type, e.g. report or export"
-    exit 1
-fi
-xcrun llvm-cov "${TYPE}" .build/x86_64-apple-macosx/debug/PFToolboxPackageTests.xctest/Contents/MacOS/PFToolboxPackageTests \
-              -instr-profile=.build/x86_64-apple-macosx/debug/codecov/default.profdata \
-              -ignore-filename-regex=".build|Tests"
+
+BUILD_PATH="./.build/"
+PACKAGE_NAME="PFToolbox"
+
+#
+# Extracts the LLVM code coverage as either a tabular report or export (JSON)
+#
+extractCoverage() {
+  xcrun llvm-cov $1 ${BUILD_PATH}/x86_64-apple-macosx/debug/${PACKAGE_NAME}PackageTests.xctest/Contents/MacOS/${PACKAGE_NAME}PackageTests \
+    -instr-profile=${BUILD_PATH}/x86_64-apple-macosx/debug/codecov/default.profdata \
+    -ignore-filename-regex="${BUILD_PATH}|Tests"
+}
+
+# Print out the tabular report
+extractCoverage report
+
+# Convert the exported JSON into our own format and save it as coverage.json
+extractCoverage export | \
+  jq '.data[0].totals.functions.percent' | \
+  awk '{printf("%.2f%%",$1)}' | \
+  ( percentage=($(< /dev/stdin)); echo "{\"schemaVersion\": 1, \"label\": \"Coverage\", \"message\": \"${percentage}\", \"color\": \"orange\"}" ) \
+  > coverage.json
