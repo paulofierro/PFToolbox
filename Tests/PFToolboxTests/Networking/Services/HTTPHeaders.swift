57cbf836e3478e1494de0a8cdc2f5921059f80e1
Paulo Fierro <paulo@paulofierro.com>
2026-03-23 18:08:27 -0500

    Added missing HTTP header tests

--- Tests/PFToolboxTests/Networking/Services/HTTPHeaders.swift
+++ Tests/PFToolboxTests/Networking/Services/HTTPHeaders.swift
@@ -7,8 +7,19 @@
 import Testing
 
 struct HTTPHeadersTests {
-    @Test func equality() {
+    @Test func headerFieldRawValues() {
+        #expect(HTTPHeaderField.accept.rawValue == "Accept")
+        #expect(HTTPHeaderField.contentType.rawValue == "Content-Type")
+        #expect(HTTPHeaderField.userAgent.rawValue == "User-Agent")
+        #expect(HTTPHeaderField.authorization.rawValue == "Authorization")
+        #expect(HTTPHeaderField.pragma.rawValue == "Pragma")
+        #expect(HTTPHeaderField.cacheControl.rawValue == "Cache-Control")
+        #expect(HTTPHeaderField.acceptEncoding.rawValue == "Accept-Encoding")
+    }
+
+    @Test func headerValueRawValues() {
         #expect(HTTPHeaderValue.jsonContent.rawValue == "application/json")
+        #expect(HTTPHeaderValue.urlEncodedFormContent.rawValue == "application/x-www-form-urlencoded")
         #expect(HTTPHeaderValue.basicAuthorization(token: "XYZ").rawValue == "Basic XYZ")
         #expect(HTTPHeaderValue.bearer(token: "XYZ").rawValue == "Bearer XYZ")
         #expect(HTTPHeaderValue.safariUserAgent.rawValue == "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15")
