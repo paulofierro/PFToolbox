b28db2616c8d29c36843f4c29b73bfdd8f0bd7e6
Paulo Fierro <paulo@paulofierro.com>
2026-03-24 09:27:53 -0500

    Added extension for cross platform hover effect

---
+++ Sources/PFToolbox/Extensions/View+HoverHighlight.swift
@@ -0,0 +1,24 @@
+//
+//   View+HoverHighlight.swift
+//   Copyright © Paulo Fierro. All rights reserved.
+//
+
+import SwiftUI
+
+public extension View {
+    /// visionOS: adds a rounded rectangle interaction and hover effect
+    /// tvOS: Adds a hover effect
+    /// Other: No-op
+    @ViewBuilder
+    func roundedRectangleHoverEffect(cornerRadius: CGFloat = 10) -> some View {
+        #if os(visionOS)
+        contentShape(.interaction, RoundedRectangle(cornerRadius: cornerRadius))
+            .contentShape(.hoverEffect, RoundedRectangle(cornerRadius: cornerRadius))
+            .hoverEffect(.highlight)
+        #elseif os(tvOS)
+        hoverEffect(.highlight)
+        #else
+        self
+        #endif
+    }
+}
