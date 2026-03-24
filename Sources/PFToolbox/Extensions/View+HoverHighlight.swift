c0d93317d656546bf685837c227e3a036f4bb15d
Paulo Fierro <paulo@paulofierro.com>
2026-03-24 09:47:57 -0500

    Added availability for hover effect

--- Sources/PFToolbox/Extensions/View+HoverHighlight.swift
+++ Sources/PFToolbox/Extensions/View+HoverHighlight.swift
@@ -10,6 +10,7 @@ public extension View {
     /// tvOS: Adds a hover effect
     /// Other: No-op
     @ViewBuilder
+    @available(iOS 13, macOS 12, visionOS 1, tvOS 18, *)
     func roundedRectangleHoverEffect(cornerRadius: CGFloat = 10) -> some View {
         #if os(visionOS)
         contentShape(.interaction, RoundedRectangle(cornerRadius: cornerRadius))
