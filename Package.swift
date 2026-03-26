a78f52af30a8ab87e81bb03a0d79d39b55606ee6
Paulo Fierro <paulo@paulofierro.com>
2026-03-26 12:24:11 -0500

    Bumped tooling

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,9 @@ let package = Package(
     name: name,
     platforms: [
         .macOS(.v12),
-        .iOS(.v13)
+        .iOS(.v13),
+        .tvOS(.v18),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -45,5 +47,6 @@ let package = Package(
                 .process("Resources")
             ]
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
